73428991a46a4589ba87cb9d26f8b259be0bfc2d
Yatekii <Yatekii@users.noreply.github.com>
2023-01-02 00:39:17 +0000

    Deploying to gh-pages from @ probe-rs/log-viewer@3bb3500db60277892745d12ddf02a944f8033ea5 🚀

--- log-viewer-d871c17458a1fe23.js
+++ log-viewer-d871c17458a1fe23.js
@@ -948,7 +948,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('log-viewer-7dd5202212b9ac0f_bg.wasm', import.meta.url);
+        input = new URL('log-viewer-d871c17458a1fe23_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
